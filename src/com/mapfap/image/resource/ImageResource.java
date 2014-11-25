3c501b41949f3e7cb9b71e6240c384d34e18181c
mapfap <mapkabb@gmail.com>
2014-11-25 21:26:36 +0700

    detect abnomal size

--- src/com/mapfap/image/resource/ImageResource.java
+++ src/com/mapfap/image/resource/ImageResource.java
@@ -11,14 +11,19 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.CacheControl;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
+import org.eclipse.jetty.http.HttpStatus;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
 
 @Singleton
 @Path("/images")
@@ -26,28 +31,26 @@ public class ImageResource {
 
 	@Context
 	UriInfo uriInfo;
-	private CacheControl cacheControl;
 
 	public ImageResource() {
-		cacheControl = new CacheControl();
-		cacheControl.setMaxAge( 86400 );
+		System.load(new File("/usr/local/share/OpenCV/java/libopencv_java2410.dylib").getAbsolutePath());
 	}
+	
+	public static final String FILE_STORAGE = "images/";
 	
 	@POST
 	@Path("")
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	public Response storeImage(@FormDataParam("file") byte[] bytes, @FormDataParam("file") FormDataContentDisposition contentDispositionHeader) {
-		String filePath = "images/" + contentDispositionHeader.getFileName();
-		FileOutputStream out;
+		String filePath = FILE_STORAGE + contentDispositionHeader.getFileName();
 		try {
-			out = new FileOutputStream(filePath);
+			FileOutputStream out = new FileOutputStream(filePath);
 			out.write(bytes);
 			out.close();
 		} catch (Exception e) {
 			e.printStackTrace();
+			throw new WebApplicationException();
 		}
-		
-//		Mat source = Highgui.imread(file, Highgui.CV_LOAD_IMAGE_COLOR);
 		
 		URI uri = null;
 		try {
@@ -57,13 +60,25 @@ public class ImageResource {
 		}
 		return Response.created(uri).build();
 	}
-	
 
 	@GET 
 	@Path("{id : \\d+}/{width : \\d+}x{height : \\d+}")
 	@Produces({ "image/png" })
 	public Response getImage(@PathParam("id") long id, @PathParam("width") int width, @PathParam("height") int height) {
-		File f = new File("test.png");
-		return Response.ok(f).build();
+		if (width * height == 0) {
+			return Response
+					.status(HttpStatus.BAD_REQUEST_400)
+					.entity("width or height can't be zero")
+					.type(MediaType.TEXT_PLAIN)
+					.build();
+		}
+		
+		Mat original = Highgui.imread("test.png");
+		Mat result = new Mat();
+		Size size = new Size(width, height);
+		Imgproc.resize(original, result, size);
+		Highgui.imwrite("test2.png", result);
+		
+		return Response.ok(new File("test2.png")).build();
 	}
 }
